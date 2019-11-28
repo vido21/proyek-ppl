fc60808d4dd261f1330e076bbf44ae596ec142c0
vido21 <vidobae@gmail.com>
2019-11-28 22:11:48 +0700

    coba cicd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Barang yang tersedia !!!!!!!</div>
+                <div class="card-header">Barang yang tersedia</div>
 
                 @foreach ($items as $item) 
                 <div class="card-body">
